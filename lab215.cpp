e771ce291026b67740040f49b791cb436de94a29
Baukash <156778292+Baukash@users.noreply.github.com>
2026-02-04 18:01:34 +0500

    Create lab215.cpp

---
+++ lab215.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() 
+{
+  int n, a, sum=0;
+  
+  cin>>n;
+  
+  for(int i=0; i<n; i++){
+    cin>>a;
+    sum+=a;
+  }
+  
+  cout<<sum;
+  
+  
+}
